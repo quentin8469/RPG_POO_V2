5b75277cee51ddcfb6faabfcce4276c87d9bbfa4
Quentin L <72231575+quentin8469@users.noreply.github.com>
2021-04-12 17:34:31 +0200

    Create the class Personnage with the constructor

---
+++ personnage.py
@@ -0,0 +1,9 @@
+class Personnage:
+    """
+    docstring
+    """
+
+    def __init__(self, name):
+        """ initialation of a person """
+        self.name = name
+        print(f"coucou je suis {self.name}")
