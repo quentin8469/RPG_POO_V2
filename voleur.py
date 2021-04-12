397443597134ae40070677c245b87d52a156d728
Quentin L <72231575+quentin8469@users.noreply.github.com>
2021-04-13 00:15:51 +0200

    add function steal

--- voleur.py
+++ voleur.py
@@ -1,12 +1,17 @@
-from personnage import Personnage
+from personnage import Person
 
 
-class Rogue(Personnage):
+class Rogue(Person):
     """
     docstring
     """
 
+    max_life = 300
+
     def __init__(self, name):
         """ initialitation of a rogue"""
-        super().__init__(name)
-        print(f"I am {self.name} a rogue")
+        super().__init__(name, 300, "Voleur", 15, 12, 15)
+
+    def steal(self):
+        """ steal a potion """
+        print(f"{self.name} à volé la potion")
