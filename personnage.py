ea599fc3858afc8060c6351e32d9bef6110b3239
Quentin L <72231575+quentin8469@users.noreply.github.com>
2021-04-12 18:07:05 +0200

    Add function presentation

--- personnage.py
+++ personnage.py
@@ -3,7 +3,17 @@ class Personnage:
     docstring
     """
 
-    def __init__(self, name):
+    max_life = 100
+
+    def __init__(
+        self,
+        name,
+        life=100,
+    ):
         """ initialation of a person """
         self.name = name
+        self.life = life
         print(f"coucou je suis {self.name}")
+
+    def presentation(self):
+        print(f"Nom: {self.name}, Points de vie: {self.life}")
