9d4617a691046e4de93718a38a8f3b36c0ea736a
Quentin L <72231575+quentin8469@users.noreply.github.com>
2021-04-12 17:37:22 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from personnage import Personnage
+from guerrier import Warrior
+from mage import Wizzard
+from voleur import Rogue
+
+'''
+def main():
+    """ docstring"""
+    return
+'''
+
+if __name__ == "__main__":
+    """ """
+    perso_1 = Personnage("bill")
+    perso_2 = Warrior("bob")
+    perso_3 = Wizzard("Bryan")
+    perso_4 = Rogue("bob")
