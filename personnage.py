124466b7cbc91ed1bad6860dd8a1274f7fd0fa8c
Quentin L <72231575+quentin8469@users.noreply.github.com>
2021-04-13 17:06:27 +0200

    Add initiative in attributs and coding the attacks function

--- personnage.py
+++ personnage.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Person:
     """
     docstring
@@ -14,15 +17,31 @@ class Person:
         self.classe = classe
         self.strength = strength
         self.intelligence = intelligence
+        self.init = random.randrange(1, 10)
         self.mana = mana
 
     def presentation(self):
-        print(f"Nom: {self.name}, Classe: {self.classe}, Points de vie: {self.life}")
+        print(
+            f"Nom: {self.name}, Classe: {self.classe}, Points de vie: {self.life}, initiative:{self.init}"
+        )
 
     def attacks(self, ennemy):
         """ function for attacks an ennemy """
-        print(f"{self.name} attack {ennemy.name}")
+        if self.classe == "":
+            degats = self.strength
+            return degats
+        if self.classe == "Guerrier":
+            degats = self.strength * 2
+            return degats
+        if self.classe == "Mage":
+            degats = self.mana * 2
+            return degats
+        if self.classe == "Voleur":
+            degats = self.strength + self.intelligence
+            print(f"{self.name} attack {ennemy.name} and do {degats} PV")
+            return degats
 
     def protection(self):
         """ function for attacks protection """
+
         print(f"{self.name} se protege")
