641c3c4f5c72fe3d242eb64c208af3871b84eb15
Quentin L <72231575+quentin8469@users.noreply.github.com>
2021-04-14 00:56:18 +0200

    refactoring of battle function, add loop while

--- arene.py
+++ arene.py
@@ -12,20 +12,32 @@ class Arena:
         """ initialisation of the arena """
         self.player1 = player1
         self.player2 = player2
-        print(f"Bonjour {player1.name} et {player2.name} pret pour le combat???")
+        print(
+            f"Bonjour {player1.name} le {player1.classe} et {player2.name} le \
+{player2.classe} pret pour le combat ???"
+        )
 
     def battle(self):
         """ start to fight"""
-        print("le combat commence")
-        if self.player1.init > self.player2.init:
-            print(f"coucou {self.player1.name} tu a l'initiative")
-            self.player1.attacks(self.player2)
-            print(
-                f"{self.player1.name} attack {self.player2.name} and do {self.player1.attacks(self.player2)} PV"
-            )
-        else:
-            print(f"coucou {self.player2.name} tu a l'initiative")
-            self.player1.attacks(self.player2)
-            print(
-                f"{self.player2.name} attack {self.player1.name} and do {self.player2.attacks(self.player1)} PV"
-            )
+        print("Tout les combattants sont dans l'arene, le combat peut commencer")
+        while self.player1.life > 0 and self.player2.life > 0:
+            self.player1.init = random.randrange(1, 10)
+            self.player2.init = random.randrange(1, 10)
+
+            if self.player1.init > self.player2.init:
+                print(f"{self.player1.name} tu a l'initiative")
+                damage = self.player1.attacks(self.player2)
+                print(
+                    f"{self.player1.name} attack {self.player2.name} and do \
+{damage} of damages"
+                )
+                self.player2.damage(damage)
+
+            else:
+                print(f"{self.player2.name} tu a l'initiative")
+                damage = self.player2.attacks(self.player1)
+                print(
+                    f"{self.player2.name} attack {self.player1.name} and do \
+{damage} of damages"
+                )
+                self.player1.damage(damage)
