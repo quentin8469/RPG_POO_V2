725de25da1b89c8cbb97fd139615a0e94365dcbd
Quentin L <72231575+quentin8469@users.noreply.github.com>
2021-04-14 17:36:26 +0200

    Add function Drink potions

--- guerrier.py
+++ guerrier.py
@@ -1,12 +1,22 @@
-from personnage import Personnage
+from personnage import Person
 
 
-class Warrior(Personnage):
+class Warrior(Person):
     """
     docstring
     """
 
+    max_life = 400
+
     def __init__(self, name):
         """ initialiation of a warrior """
-        super().__init__(name)
-        print(f"I am {self.name} a warrior")
+        super().__init__(name, 400, "Guerrier", 20, 10, 15)
+
+    def drink_potion(self, potion):
+        """ drink heal potion """
+        if self.life < Warrior.max_life:
+            print(Warrior.max_life)
+            print("Besoin de potions")
+        else:
+            # print(Warrior.max_life)
+            print("pas besoin de potions")
