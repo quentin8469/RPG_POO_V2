13ab13707b3a93e866fd2486ae19fea6455a59a1
Quentin L <72231575+quentin8469@users.noreply.github.com>
2021-04-14 17:35:09 +0200

    Add function drink potion

--- mage.py
+++ mage.py
@@ -1,12 +1,22 @@
-from personnage import Personnage
+from personnage import Person
 
 
-class Wizzard(Personnage):
+class Wizzard(Person):
     """
     docstring
     """
 
+    max_life = 200
+
     def __init__(self, name):
         """ initialitation of a wizzard"""
-        super().__init__(name)
-        print(f"I am {self.name} a wizzard")
+        super().__init__(name, 200, "Mage", 5, 20, 20)
+
+    def drink_potion(self, potion):
+        """ drink heal potion """
+        if self.life < Wizzard.max_life:
+            print(Wizzard.max_life)
+            print("Besoin de potions")
+        else:
+            # print(Wizzard.max_life)
+            print("pas besoin de potions")
