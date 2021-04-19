90a742013850ca8487177d351d10f886432aeef0
Quentin L <72231575+quentin8469@users.noreply.github.com>
2021-04-19 20:34:05 +0200

    delette print in the class attribut

--- arene.py
+++ arene.py
@@ -6,21 +6,19 @@ class Arena:
     docstring
     """
 
-    print("L'Arene est crée")
-
     def __init__(self, player1, player2):
         """ initialisation of the arena """
         self.player1 = player1
         self.player2 = player2
         print(
             f"Bonjour {player1.name} le {player1.classe} et {player2.name} le \
-{player2.classe} pret pour le combat ???"
+{player2.classe} prêt pour le combat ???"
         )
 
     def battle(self):
         """ start to fight"""
 
-        print("Tout les combattants sont dans l'arene, le combat peut commencer!!")
+        print("Tout les combattants sont dans l'arène, le combat peut commencer!!")
         round_nb = 0
         while self.player1.life > 0 and self.player2.life > 0:
             round_nb += 1
@@ -30,7 +28,7 @@ class Arena:
 
             if self.player1.init > self.player2.init:
                 print(f"{self.player1.name} tu a l'initiative")
-                damage = self.player1.attacks(self.player2)
+                damage = self.player1.attacks()
                 print(
                     f"{self.player1.name} attack {self.player2.name} and do \
 {damage} of damages"
@@ -39,7 +37,7 @@ class Arena:
 
             else:
                 print(f"{self.player2.name} tu a l'initiative")
-                damage = self.player2.attacks(self.player1)
+                damage = self.player2.attacks()
                 print(
                     f"{self.player2.name} attack {self.player1.name} and do \
 {damage} of damages"
