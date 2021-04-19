7d220d93d16ecf28d5a038d30f2895af89cb17d6
Quentin L <72231575+quentin8469@users.noreply.github.com>
2021-04-19 20:36:03 +0200

    modif drink_potion. now this function remove hp for the wizard

--- mage.py
+++ mage.py
@@ -1,23 +1,23 @@
 from personnage import Person
 
 
-class Wizzard(Person):
+class Wizard(Person):
     """
-    Create a Wizzard
+    Create a Wizard
     """
 
     max_life = 200
 
     def __init__(self, name):
-        """ initialitation of a wizzard"""
+        """ initialisation of a wizard"""
         super().__init__(name, 200, "Mage", 5, 20, 20)
 
     def drink_potion(self, potion):
         """ drink heal potion """
-        if self.life < Wizzard.max_life:
-            self.life = self.life + potion.hp
-            if self.life > Wizzard.max_life:
-                self.life = Wizzard.max_life
+        if self.life < Wizard.max_life:
+            self.life = self.life - potion.hp/2
+            if self.life > Wizard.max_life:
+                self.life = Wizard.max_life
             print(self.life)
             print("vous avez pris une potions")
         else:
