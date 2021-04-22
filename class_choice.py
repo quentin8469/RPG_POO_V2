92713f4d7ef1c83e129fbebad0b9a04c11cc20e7
Quentin L <72231575+quentin8469@users.noreply.github.com>
2021-04-22 16:54:36 +0200

    Add function get_class

--- class_choice.py
+++ class_choice.py
@@ -13,6 +13,22 @@ class ChoiceClass:
         print(f"Hello! So you want play a {class_name}, good choice!!")
         return class_name
 
-    def get_class(self, player_class):
+    def get_class(self, class_name):
         """ return the class choose by the player """
-        pass
+        if class_name == "warrior":
+            from guerrier import Warrior
+
+            name = input("Enter your name: ")
+            return Warrior(name).presentation()
+        elif class_name == "rogue":
+            from voleur import Rogue
+
+            name = input("Enter your name: ")
+
+            return Rogue(name).presentation()
+        elif class_name == "wizard":
+            from mage import Wizard
+
+            name = input("Enter your name: ")
+
+            return Wizard(name).presentation()
